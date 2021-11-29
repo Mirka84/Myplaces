1cf1c672ba1feeae10babe9dadea80f48dd68206
Mirka Heikkila <mirkanms@hotmail.com>
2021-11-29 19:31:46 +0200

    'toka'

--- Map.js
+++ Map.js
@@ -1,93 +1,78 @@
 import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native'; 
+import { Alert, StyleSheet, View } from 'react-native'; 
 import{ Button } from'react-native-elements';
-import MapView, { Marker} from'react-native-maps'; 
+import MapView, { Marker } from'react-native-maps'; 
 
 
 export default function map({ route, navigation }){
 
     const{ address } = route.params;
 
-      const [initial, setInitial]=useState({
-        latitude: 60.200692,
-        longitude: 24.934302,
-        latitudeDelta: 0.0322,
-        longitudeDelta: 0.0221
-      })
-
-      const [region, setRegion]=useState(initial);
-
-      useEffect (() => {
-        fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=3JIT9JdCuHOBuHA8jsz6eLzHUoceyCUT&location=${address}`)
-        .then(response => response.json())
-        .then(responseData => {
-          console.log(responseData.results[0].locations[0].displayLatLng.lat);
-          setRegion({
-          latitude: responseData.results[0].locations[0].displayLatLng.lat,
-          longitude: responseData.results[0].locations[0].displayLatLng.lng,
-          latitudeDelta: 0.0322,
-          longitudeDelta: 0.0221
-      })
-        })
-      },[])  
-
+    const initial = {
+      latitude: 60.200692,
+      longitude: 24.934302,
+      latitudeDelta: 0.0322,
+      longitudeDelta: 0.0221
+    };
+  
+    const [region, setRegion]=useState(initial);  
     
+    const [place, setPlace]=useState('');
 
-    return (
-    <View>
-        <MapView
-        style={styles.map}
-        region={region}
-        provider="google"
-        >
-        <Marker
-          coordinate={region}
-          draggable={true}
-          onDragStart={(e)=> {
-            console.log('Started', e.nativeEvent.coordinate)
-          }}
-          onDragEnd={(e)=> {
-            setPin({
-              latitude: e.nativeEvent.coordinate.latitude,
-              longitude: e.nativeEvent.coordinate.longitude
-            })
-          }}
-          title={address}
-        />
-        </MapView>
+    const getAddress = async () => {
+      const url=`http://www.mapquestapi.com/geocoding/v1/address?key=3JIT9JdCuHOBuHA8jsz6eLzHUoceyCUT&location=${address}`;
+     
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        
+        
+        const { lat, lng }=data.results[0].locations[0].latLng;
+        console.log(place);
+        setRegion({...region, latitude: lat, longitude: lng})
+        
+        setPlace(data.results[0].providedLocation.location);
+        
+      } catch (e) {
+        Alert.alert('Error fetching data');
+      }
+      
+    } 
 
-        <Button buttonStyle={{ width: 300 }} onPress={() => navigation.navigate('MY PLACES')} title="Save the place" /> 
-    </View>
-
-    );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    paddingTop:StatusBar.currentHeight,
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  map: {
-    flex: 1,
-    width: "100%",
-    height: "100%"
-  },
-  textinput: {
-    width: 300,
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-
-  }
-});
-
-
+    useEffect(() => { getAddress() }, []); 
 
 
 
   
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          region={region}
+        >
+          <Marker
+            coordinate={region}
+            title={address}
+          />
+        </MapView>
+
+        <Button buttonStyle={{ width: 300 }} onPress={() => navigation.navigate('MY PLACES', {place: place})} title="Save the place" /> 
+    </View>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      paddingTop:StatusBar.currentHeight,
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    map: {
+      flex: 1,
+      width: "100%",
+      height: "100%"
+    }
+  });
--- Myplaces.js
+++ Myplaces.js
@@ -1,17 +1,21 @@
 import React, { useState } from 'react';
-import { View } from 'react-native'; 
+import { View, Text } from 'react-native'; 
 import{ Input, Button } from'react-native-elements';
 
-export default function myplaces({ navigation }){
+export default function myplaces({ route, navigation }){
+
+    const{ place } = route.params; 
 
     const [address, setAddress]=useState(''); 
 
+    
     return (
     <View>
         <Input placeholder='Type the address'
         onChangeText={(address) => setAddress(address)}
         value={address}/>  
         <Button buttonStyle={{ width: 300 }} onPress={() => navigation.navigate('MAP', {address: address})} title="Show on a map" /> 
+        <Text>{place}</Text>
     </View>
 
     )
